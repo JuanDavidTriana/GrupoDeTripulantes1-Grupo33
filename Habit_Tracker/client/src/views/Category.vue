ec20176c698b9198efcefabcf7b584c77e5b2f12
ratara5 <ratara5@gmail.com>
2021-10-07 14:29:28 -0500

    Obteniendo y pintando productos al por categoría

--- Habit_Tracker/client/src/views/Category.vue
+++ Habit_Tracker/client/src/views/Category.vue
@@ -1,31 +1,50 @@
 <template>
-  <BasicLayout>
-      <h2>Estamos en la categoría</h2>
-  </BasicLayout>
+  <BasicLayouts>
+    <div class="ui grid">
+      <div
+        class="sixten wide mobile eight wide tablet four wide computer column"
+        v-for="product in products"
+        :key="product.id"
+      >
+        <Product :product="product" />
+      </div>
+    </div>
+  </BasicLayouts>
 </template>
 
 <script>
-import {onMounted} from 'vue';
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
 import BasicLayouts from '../layouts/BasicLayouts.vue';
-import {getCategoriesApi} from '../api/category';
+import Product from '../components/Product.vue';
+import { getCategoriesApi, getProductsCategory } from '../api/products';
 export default {
-  name:'Category',
-  components: { BasicLayouts 
-  }, 
-  watch:{
-    $router(to,from){
-      console.log(to)
+  name: 'Category',
+  components: {
+    BasicLayouts,
+    Product,
+  },
+  watch: {
+    $route(to, from) {
+      this.getProduts(to.params.category);
     },
   },
-  setup(){
-    onMounted(async ()=>{
-      const response=await getCategoriesApi("Leisure");
-      console.log(response);
+  setup() {
+    let products = ref(null);
+    const { params } = useRoute();
+    onMounted(() => {
+      getProduts(params.category);
     });
+    const getProduts = async (category) => {
+      const response = await getProductsCategory(category);
+      products.value = response;
+    };
+    return {
+      getProduts,
+      products,
+    };
   },
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
