97af806f4e385f27f6e5d2bd8d29894436b31013
ratara5 <ratara5@gmail.com>
2021-10-07 11:10:09 -0500

    Renderizar todos los productos

--- Habit_Tracker/client/src/views/Home.vue
+++ Habit_Tracker/client/src/views/Home.vue
@@ -5,6 +5,14 @@
     <br />
     <br />
     <Testimonios />
+    <div>
+      <h1>Productos</h1>
+      <div class="ui grid">
+        <div class="sixten wide mobile eight wide tablet four wide computer column" v-for="product in products" :key="product.id">          
+          <Product :product="product"/>
+        </div>
+      </div>
+    </div>
   </BasicLayouts>
 </template>
 
@@ -15,6 +23,7 @@ import Testimonios from "../components/Testimonios.vue";
 import Cards from '../components/Cards.vue';
 import {ref, onMounted} from 'vue';
 import {getProducts} from '../api/products';
+import Product from '../components/Product.vue'
 
 export default {
   name: "Home",
@@ -22,6 +31,7 @@ export default {
     BasicLayouts,
     Testimonios,
     Cards,
+    Product,
   },
 
   setup(){
