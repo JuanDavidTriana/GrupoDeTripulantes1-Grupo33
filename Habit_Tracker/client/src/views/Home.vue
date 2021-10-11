c3020cb3c25f0c5fa0841baa3f7757606cf6e352
jegomezb <foxjuanes@gmail.com>
2021-10-11 18:31:43 -0500

    Agregando estilos

--- Habit_Tracker/client/src/views/Home.vue
+++ Habit_Tracker/client/src/views/Home.vue
@@ -3,17 +3,16 @@
     <Cards />
     <br />
     <br />
-    <div >    
-    
-    <div class="ui container">
-      <h1>Calendario</h1>
-      <datepicker
-        lang="Greg"
-        type="range"
-        colorTheme="pink"
-        v-model="dateGreg"
-      />
-    </div>
+    <div>
+      <div class="ui container">
+        <h1>Calendario</h1>
+        <datepicker
+          lang="Greg"
+          type="range"
+          colorTheme="pink"
+          v-model="dateGreg"
+        />
+      </div>
     </div>
     <br />
     <br />
@@ -21,11 +20,13 @@
     <br />
     <br />
     <div>
+      <h1>Últimos Productos</h1>
       <div class="ui grid">
         <div
           class="sixten wide mobile eight wide tablet four wide computer column"
           v-for="product in products"
-          :key="product.id">
+          :key="product.id"
+        >
           <Product :product="product" />
         </div>
       </div>
@@ -35,16 +36,16 @@
 
 <script>
 // @ is an alias to /src
-import BasicLayouts from '../layouts/BasicLayouts.vue';
-import Testimonios from '../components/Testimonios.vue';
-import Cards from '../components/Cards.vue';
-import { ref, onMounted } from 'vue';
-import { getProducts } from '../api/products';
-import Product from '../components/Product.vue';
-import datepicker from 'vue-awesome-datepicker';
+import BasicLayouts from "../layouts/BasicLayouts.vue";
+import Testimonios from "../components/Testimonios.vue";
+import Cards from "../components/Cards.vue";
+import { ref, onMounted } from "vue";
+import { getProducts } from "../api/products";
+import Product from "../components/Product.vue";
+import datepicker from "vue-awesome-datepicker";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     BasicLayouts,
     Testimonios,
@@ -75,11 +76,14 @@ export default {
 
 <style lang="scss" scoped>
 * {
-  font-family: 'Comfortaa', cursive;
+  font-family: "Comfortaa", cursive;
 }
 .ui.container {
   text-align: center;
-  color:#512d6d;
+  color: #512d6d;
   position: center;
+}
+h1 {
+  color: #512d6d;
 }
 </style>
