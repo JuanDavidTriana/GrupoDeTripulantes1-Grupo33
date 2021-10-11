d20daa6b4455d95d61606e5db96609dbf731bee7
ratara5 <ratara5@gmail.com>
2021-10-11 11:20:29 -0500

    Creando carrito, abriéndolo y cerrándolo

--- Habit_Tracker/client/src/layouts/BasicLayouts.vue
+++ Habit_Tracker/client/src/layouts/BasicLayouts.vue
@@ -6,17 +6,21 @@
       <slot />
     </div>
   </div>
+
+  <Cart />
 </template>
 
 <script>
 import Menu from "../components/Menu.vue";
 import Header from "../components/Header.vue";
+import Cart from "../components/Cart/Cart.vue";
 
 export default {
   name: "BasicLayouts",
   components: {
     Menu,
     Header,
+    Cart,
   },
 };
 </script>
